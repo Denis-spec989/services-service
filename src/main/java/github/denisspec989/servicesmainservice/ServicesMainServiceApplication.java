322e09cbf713454080b155779be0d64ec902f4f8
Denis Ignatov <dsignatov@licard.com>
2023-03-04 12:21:18 +0300

    Init commit

---
+++ src/main/java/github/denisspec989/servicesmainservice/ServicesMainServiceApplication.java
@@ -0,0 +1,13 @@
+package github.denisspec989.servicesmainservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ServicesMainServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ServicesMainServiceApplication.class, args);
+	}
+
+}
